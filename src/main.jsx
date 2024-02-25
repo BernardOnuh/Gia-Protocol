cce0ac442303efd2e269c9ebec822989d2386e0c
Bernard.O <114490070+BernardOnuh@users.noreply.github.com>
2024-02-25 16:15:15 +0100

    Update main.jsx
    Main

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,36 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { arbitrum, base, mainnet, optimism, polygon, zora,cronos } from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+
+
+
+
+const config = getDefaultConfig({
+	appName: 'RainbowKit demo',
+	projectId: 'YOUR_PROJECT_ID',
+	chains: [ polygon, cronos],
+  });
+  
+  const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
 		<Router>
 			<App />
 		</Router>
-	</React.StrictMode>,
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>,
 );
+
+
